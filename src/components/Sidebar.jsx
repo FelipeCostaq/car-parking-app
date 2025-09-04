898560aed3e9ae54a63d399474d2e122a94453d9
FelipeCostaq <felipecostasiqu@gmail.com>
2025-09-04 08:44:13 -0300

    :art: SideBar items order ajust

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,8 @@ function Header() {
     <div className="d-flex">
       <nav className="nav flex-column p-3" style={{ width: '250px', height: '100vh' }}>
         <Link className="nav-link-custom" to="/"><i className="bi bi-house-fill"></i> Home</Link>
-        <Link className="nav-link-custom" to="/spot"><i class="bi bi-p-square-fill"></i> Vagas</Link>
         <Link className="nav-link-custom" to="/car"><i class="bi bi-car-front-fill"></i> Carros</Link>
+        <Link className="nav-link-custom" to="/spot"><i class="bi bi-p-square-fill"></i> Vagas</Link>
         <Link className="nav-link-custom" to="/assignment"><i class="bi bi-pass-fill"></i> Atribuições</Link>
       </nav>
     </div>
