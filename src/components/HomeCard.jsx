3935bdf3a84905be424eb3c9878a00d0698940d7
FelipeCostaq <felipecostasiqu@gmail.com>
2025-09-04 09:35:29 -0300

    :art: component HomeCard ajust

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -2,10 +2,12 @@ import './HomeCard.css'
 
 function HomeCard(){
     return(
-        <div className='card'>
-            <h1>Bem-vindo!</h1>
-            <p>Use o menu ao lado para explorar todas as funcionalidades.</p>
-            <p><a href="https://github.com/FelipeCostaq"><i id='github-icon' className="bi bi-github color"></i></a></p>
+        <div className='d-flex justify-content-center align-items-center' style={{ height: '100vh', width: "100%" }}>
+            <div className='card' style={{ maxWidth: '600px', width: '100%' }}>
+                <h1>Bem-vindo!</h1>
+                <p>Use o menu ao lado para explorar todas as funcionalidades.</p>
+                <p><a href="https://github.com/FelipeCostaq"><i id='github-icon' className="bi bi-github color"></i></a></p>
+            </div>
         </div>
     )
 }
